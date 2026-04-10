d2ed5138c874d2a12c0460a430b4acb93b1c4ef6
Andrii Khaisin <akhaisin@gmail.com>
2026-04-09 17:45:05 -0700

    Add build configuration to Vite setup; enable source maps and disable minification

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/learning-react/',
   plugins: [react()],
+  build: {
+    // This is a demo/exercises app. Enabling original sources available in Chrome Dev Tools.
+    sourcemap: true,
+    minify: false,
+    cssMinify: false,
+  },
 });
