44b1de455de226329bb34db6430414cf77da6bfb
Andrii Khaisin <akhaisin@gmail.com>
2026-04-10 03:29:24 -0700

    Add StarRating component with styling and functionality; implement star selection and hover effects

---
+++ src/pages/starRating/StarRatingDemo.tsx
@@ -0,0 +1,7 @@
+import StarRating from '../../components/starRating/StarRating';
+
+function StarRatingDemo() {
+  return <StarRating maxStars={5} initialSelection={2} />;
+}
+
+export default StarRatingDemo;
